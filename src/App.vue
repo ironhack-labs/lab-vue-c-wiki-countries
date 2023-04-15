04f3030a17b1dc5027b605121ac326dbdccdbc40
Pablo <pablobeneyto1@gmail.com>
2023-04-15 12:52:14 +0200

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,70 @@
-<template>
+<template> 
   <div class="app">
-
+    <Navbar/>
+    <div className="container">
+      <router-view/>   
+    </div>
   </div>
 </template>
 
 <script>
-
+   import Navbar from "./components/Navbar.vue";
+   export default {
+      data: () => ({
+        
+      }),
+      components: { Navbar },
+   }
 </script>
 
-<style>
 
+<style>
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+ul {
+  list-style: none;
+}
+.app {
+  text-align: center;
+}
+.app-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
+@media (prefers-reduced-motion: no-preference) {
+  .app-logo {
+    animation: app-logo-spin infinite 20s linear;
+  }
+}
+.app-header {
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+.app-link {
+  color: #61dafb;
+}
+@keyframes app-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
 </style>
---
+++ src/components/CountryDetails.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="col-7">
+      <img
+        v-bind:src="`https://flagpedia.net/data/flags/w702/${alpha2Code.toLowerCase()}.png`"
+        alt="country flag"
+        style="width: 300px"
+      />
+      <h1>{{ name }}</h1>
+      <table class="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style="width: 30%">Capital</td>
+            <td>{{ capital }}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>{{ area }} km <sup>2</sup></td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <p v-if="borders.length === 0">
+                Este pais no tiene paises fronterizos.<br/>Quizas se trata de una isla...
+              </p>
+              <ul v-else>
+                <li v-for="(border, index) in borders" :key="index">
+                  <router-link :to="`/list/${border}`">{{ border }}</router-link>
+                </li>
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+    
+  export default {
+    name: "CountryDetails",
+    data() {
+      return {
+        name: "",
+        capital: "",
+        alpha3Code: "",
+        alpha2Code: "",
+        area: "",
+        borders: [],
+        countryInfo: {},
+      };
+    },
+    methods: {
+      async getCountryByAlphaCode() {
+        console.log("estoy dentro de getCountryByAlphaCode");
+        this.alpha3Code = this.$route.params.alpha3Code;
+        const response = await fetch(
+          `https://ih-countries-api.herokuapp.com/countries/${this.alpha3Code}`
+        );
+        
+        const finalResponse = await response.json();
+        console.log(finalResponse);
+        this.name = finalResponse.name.common;
+        this.capital = finalResponse.capital[0];
+        this.area = finalResponse.area;
+        this.borders = finalResponse.borders;
+        this.alpha2Code = finalResponse.alpha2Code;
+        this.countryInfo = finalResponse;
+      },
+    },
+    mounted() {
+      this.getCountryByAlphaCode();
+    },
+    computed: {
+      countryCode() {
+        return this.$route.params.alpha3Code;
+      },
+    },
+    watch: {
+      countryCode(newCountryCode) {
+        this.getCountryByAlphaCode();
+      },
+    },
+  };
+  </script>
+  
+  <style></style>
