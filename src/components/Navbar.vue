513fe46a3adf2d8a23a59f56f6af1e7b8e942f0c
edankap <edankap20@gmail.com>
2023-04-22 10:12:46 +0200

    Project Done

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="navbar navbar-dark bg-primary mb-3">
+        <div class="container">
+            <RouterLink class="navbar-brand" to="/">Lab-WikiCountries</RouterLink>
+        </div>
+    </nav>
+  </template>
+  
+  <script>
+  import { RouterLink } from 'vue-router';
+  export default {
+    name: "Navbar",
+    components: {
+      RouterLink,
+    }
+  };
+  </script>
+  
+  <style>
+  </style>
