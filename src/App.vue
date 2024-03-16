5b21f1f3eb2b149f05b834670604a7b98bf87456
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-03-16 14:24:11 +0100

    basic setup for components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
+<script setup>
+import CountriesList from '@/components/CountriesList.vue'
+</script>
+
 <template>
   <div class="app">
-
+    <h1>Hello</h1>
+    <CountriesList />
   </div>
 </template>
 
-<script>
-
-</script>
-
-<style>
+<style scoped>
 
 </style>
