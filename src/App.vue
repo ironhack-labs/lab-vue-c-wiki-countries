b87fe210feb7d67e83caef5cdc76ab7bc17a4481
Karen Schneider <karen-schneider@outlook.com>
2024-03-16 15:01:52 +0100

    primera iteracion

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
-<script setup></script>
+<script setup>
+import CompNavbar from "@/components/CompNavbar.vue";
+import CountriesList from "@/components/CountriesList.vue";
+import CountryDetails from "./components/CountryDetails.vue";</script>
 
-<template></template>
+<template>
+   <CompNavbar />
+  <CountriesList/>
+  <CountryDetails/>
+</template>
 
 <style scoped></style>
---
+++ src/components/CompNavbar.vue
@@ -0,0 +1,7 @@
+
+
+<template>
+  <nav class="navbar navbar-dark bg-primary mb-3">
+    <a class="navbar-brand" href="/">WikiCountries</a>
+  </nav>
+</template>
