4745141e1e4da6a125529cb8a28903229f1d7ea4
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-03-18 20:23:32 +0100

    remove unused imports

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <script setup>
 
 import Navbar from '@/components/Navbar.vue'
-import CountriesList from '@/components/CountriesList.vue'
-import CountryDetails from '@/components/CountryDetails.vue'
-
 
 </script>
 
 <template>
   <div class="app">
     <Navbar />
-    <div class="components">
       <router-view />
-  </div>
   </div>
 </template>
 
 <style scoped>
 @import "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css";
-
-.components {
-  display: flex;
-  flex-direction: row;
-}
 
 </style>
