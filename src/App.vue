070f765e6a6507e0b7b0942f6e6529248b5e8ac3
Max Freixa <freixa.sureda.max@alumnat.copernic.cat>
2024-03-19 19:52:26 +0100

    intertation 1, 1.1, 1.2

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,48 @@
-<template>
-  <div class="app">
+<script setup>
 
-  </div>
-</template>
-
-<script>
+import { ref } from 'vue';
+import Navbar from './components/Navbar.vue';
+import CountryDetails from './components/CountryDetails.vue';
+import CountriesList from './components/CountriesList.vue';
 
 </script>
 
-<style>
+<template>
 
-</style>
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+  </head>
+
+  <body>
+    <div class="app">
+      <!-- Example of what could be rendered -->
+
+      <div>
+        <!-- Navbar -->
+        <Navbar title="LAB -" />
+        <!-- Bootstrap container wrapper div -->
+        <div class="container">
+          <!-- Bootstrap row wrapper div -->
+          <div class="row">
+
+            <CountriesList />
+
+            <CountryDetails />
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+
+  </html>
+
+</template>
+
+<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    title: String
+});
+
+
+</script>
+
+<template>
+    <nav class="navbar navbar-dark bg-primary mb-3">
+        <div class="container">
+            <a class="navbar-brand" href="/">{{ title }} WikiCountries</a>
+        </div>
+    </nav>
+</template>
+
+<style></style>
