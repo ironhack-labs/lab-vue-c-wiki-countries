f3f92f67bd18b828dafb1a90603c1d86aeb4a16b
Laura Musso <lauramussopd@gmail.com>
2024-03-20 18:29:52 +0100

    wed 1829

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,29 +1,33 @@
 <script setup>
 
-import { ref } from 'vue';
+import { ref, onMounted,watch } from 'vue';
 
-const countries = ref({});
+const countries = ref([]); //definisce una variabile reattiva, memorizza i dati dei paesi ottenuti dall'api
 
-async function _getCountryData() {
-    const res = await fetch("https://ih-countries-api.herokuapp.com/countries");
-    const json = await res.json();
-    countries.value = json;
+const _getCountryList = async () => { //definiamo una funzione asincrona
+    const response = await fetch('https://ih-countries-api.herokuapp.com/countries'); //fa una richiesta get utilizzando la funzione fetch e guarda dentro a responde il risultato della richiesta 
+    const data = await response.json(); //prende la responde e la converte in formato json e la salva dentro a data
+    countries.value = data.sort((a, b) => a.name.common.localeCompare(b.name.common));
 }
 
-console.log(_getCountryData())
+onMounted(() => {
+    _getCountryList();
+});
 
 </script>
 
 <template>
-<div class="row ml-3">
-    <section class="col-5" style="max-height: 90vh; overflow: scroll">
-
-        <router-link v-for="country in countries" :key="country.alpha3Code" :to="`/list/${country.alpha3Code}`">
-            <img :src="`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`" style="width: 50px" />
-            <p>{{ country.name.common }}</p>
-        </router-link>
-    </section>
-</div>
+    <div class="row ml-3">
+        <section class="col-5" style="max-height: 90vh; overflow: scroll">
+            <div class="list-group">
+                <router-link v-for="country in countries" :key="country.alpha3Code" :to="`/list/${country.alpha3Code}`" class="list-group-item">
+                    <img :src="`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`"
+                        style="width: 50px" />
+                    <p>{{ country.name.common }}</p>
+                </router-link>
+            </div>
+        </section>
+    </div>
 
 </template>
 
@@ -32,4 +36,8 @@ a {
     text-decoration: none;
     color: black;
 }
+.list-group-item {
+  padding: 0;
+}
+
 </style>
--- src/components/CountryDetails.vue
+++ src/components/CountryDetails.vue
@@ -1,8 +1,67 @@
 <script setup>
 
-</script>
+import { ref, onMounted, watch } from 'vue'; //importa le funzioni necessarie da vue e da vue router
+import { useRoute } from 'vue-router';
 
+const route = useRoute(); // funzione che si utilizza per otternere d'oggetto della route attuale
+const country = ref(null); //si definisce la variabile reattiva country, che sara utilizzata nel componente per memorizzare info relative al paese corrente
+
+const fetchCountryDetails = async (alpha3Code) => {
+  alpha3Code = alpha3Code.toUpperCase();
+  try {
+    const response = await fetch('https://ih-countries-api.herokuapp.com/countries');
+    const data = await response.json();
+    country.value = data.find(c => c.alpha3Code === alpha3Code);
+  } catch (error) {
+    console.error("Error fetching country details:", error);
+  }
+};
+onMounted(() => {
+  fetchCountryDetails(route.params.alpha3Code);
+});
+
+watch(() => route.params.alpha3Code, (newAlpha3Code) => {
+  fetchCountryDetails(newAlpha3Code);
+});
+
+watch(country, () => {
+  console.log(country); 
+});
+
+</script>
 <template>
+    <div class="col-7" v-if="country">
+       <img
+        :src="`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`"
+        :alt="`Flag of ${country.name.common}`"
+        style="width: 50px"
+      /> 
+      
+      <h1>{{ country.name.common }}</h1>
+      <table class="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style="width: 30%">Capital</td>
+            <td>{{ country.capital[0] }}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>{{ country.area }} km <sup>2</sup></td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+                <li v-for="border in country.borders" :key="border">{{ border }}</li>
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+<!--<template>
     <div class="row">
         <section class="col-7">
             <h1>HOLA</h1>
@@ -11,3 +70,4 @@
 </template>
 
 <style scoped></style>
+-->
