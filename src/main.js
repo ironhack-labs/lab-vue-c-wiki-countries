62d0c8abd4b7e01bf977fd91aeca0c4b399f41c5
Auba Fuster <aubafp@gmail.com>
2024-03-22 21:07:23 +0100

    fix project execution but importing pinia correctly. All working

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-// src/main.js
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
+
 import router from "./router";
 
 import '@/assets/main.css'
 
+const pinia = createPinia()
+
 createApp(App)
-    .use(router)
-    .mount("#app");
+  .use(pinia)
+  .use(router)
+  .mount("#app");
