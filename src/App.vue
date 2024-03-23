cf2c947a8c242b94140fb7132ccb19f2681b00da
Joan Vila Valls <joan.o.jan@gmail.com>
2024-03-23 19:01:20 +0100

    the basic countries with static json

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<script setup>
+import NavBar from "@/components/Navbar.vue";
+</script>
+
 <template>
-  <div class="app"></div>
+  <div class="app">
+    <NavBar />
+    <router-view />
+  </div>
 </template>
